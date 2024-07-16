ac01640dc6c508093cad089418e996750f15234e
jimin9703 <moses0322@naver.com>
2024-07-16 15:44:28 +0900

    feat: [김지민] board 껍데기 [CGTP]

---
+++ gtp_backend/board/controller/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from board.entity.models import Board
+from board.serializers import BoardSerializer
+from board.service.board_service_impl import BoardServiceImpl
+
+
+# Create your views here.
+class BoardView(viewsets.ViewSet):
+    queryset = Board.objects.all()
+    boardService = BoardServiceImpl.getInstance()
+
+    def list(self, request):
+        boardList = self.boardService.list()
+        serializer = BoardSerializer(boardList, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
