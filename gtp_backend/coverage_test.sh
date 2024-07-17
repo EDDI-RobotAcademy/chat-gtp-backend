f98c776942f48f02163f5a7e78a14fe46d2d2a77
jimin9703 <moses0322@naver.com>
2024-07-17 15:30:43 +0900

    feat:[김지민] 자동화스크립트 구성 [CGTP]

---
+++ gtp_backend/coverage_test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+TEST_DIRS=$(find . -path ./.venv -prune -o -type d -name 'tests' -print)
+
+source .venv/Scripts/activate
+
+for TEST_DIR in $TEST_DIRS; do
+  APP_NAME=$(basename $(dirname $TEST_DIR))
+  TEST_FILES=$(find $TEST_DIR -type f -name 'test*.py')
+
+  if [ -z "$TEST_FILES" ]; then
+    echo "No test files found in $TEST_DIR"
+    continue
+  fi
+
+  echo "Running tests for $APP_NAME"
+  coverage run --source='.' manage.py test ${APP_NAME}.tests
+done
+
+coverage combine
+coverage report
+coverage html
+coverage xml
