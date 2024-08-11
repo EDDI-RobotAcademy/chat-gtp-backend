43cea5f1274eeb2d611d4b88bf737aa47b93d04e
younghyen7956 <155882166+younghyen7956@users.noreply.github.com>
2024-08-11 13:36:30 +0900

    feat: [김용현] naver 토큰 redis 연동 and 계정 등록 [PROJ-DJANGO-18]

--- gtp_backend/naver_oauth/controller/views.py
+++ gtp_backend/naver_oauth/controller/views.py
@@ -9,10 +9,12 @@ from account.service.account_service_impl import AccountServiceImpl
 from naver_oauth.serializer.naver_oauth_access_token_serializer import NaverOauthAccessTokenSerializer
 from naver_oauth.serializer.naver_oauth_url_serializer import NaverOauthUrlSerializer
 from naver_oauth.service.naver_oauth_service_impl import NaverOauthServiceImpl
+from naver_oauth.service.redis_service_impl import RedisServiceImpl
 
 
 class NaverOauthView(viewsets.ViewSet):
     naverOauthService = NaverOauthServiceImpl.getInstance()
+    redisService = RedisServiceImpl.getInstance()
     accountService = AccountServiceImpl.getInstance()
 
     def naverOauthURI(self, request):
@@ -43,3 +45,34 @@ class NaverOauthView(viewsets.ViewSet):
             return JsonResponse({'user_info': user_info})
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=500)
+
+    def redisNaverAccessToken(self, request):
+        try:
+            email = request.data.get('email')
+            access_token = request.data.get('naverAccessToken')
+            print(f"redisAccessToken -> email: {email}")
+
+            account = self.accountService.findAccountByEmail(email)
+            if not account:
+                return Response({'error': 'Account not found'}, status=status.HTTP_404_NOT_FOUND)
+
+            userToken = str(uuid.uuid4())
+            self.redisService.store_access_token(account.id, userToken)
+            accountId = self.redisService.getValueByKey(userToken)
+            print(f"accountId: {accountId}")
+
+            return Response({ 'userToken': userToken }, status=status.HTTP_200_OK)
+        except Exception as e:
+            print('Error storing access token in Redis:', e)
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def dropRedisTokenForLogout(self, request):
+        try:
+            userToken = request.data.get('userToken')
+            isSuccess = self.redisService.deleteKey(userToken)
+
+            return Response({'isSuccess': isSuccess}, status=status.HTTP_200_OK)
+        except Exception as e:
+            print('레디스 토큰 해제 중 에러 발생:', e)
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
